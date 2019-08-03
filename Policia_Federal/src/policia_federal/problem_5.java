f90bda334f6f94b67aa75359cbc0deaa2e7dc40f
SmallHDMax <50238083+SmallHDMax@users.noreply.github.com>
2019-08-03 00:31:45 -0300

    Update problem_5.java

--- Policia_Federal/src/policia_federal/problem_5.java
+++ Policia_Federal/src/policia_federal/problem_5.java
@@ -3,14 +3,14 @@ package policia_federal;
 public class problem_5 {
 
     public void menorNumero() {
-        boolean continua = true;
+        boolean continua = true;//Variavel para continuar o do while
         int num = 2, menor = 0;
         do {
-            for (int div = 20; div >= 0; div--) {
-                if (div == 0) {
+            for (int div = 20; div >= 0; div--) {//For para fazer a divisão dos números
+                if (div == 0) {//caso a div chegue a 0 significa que o número é divisivel pelos valores
                     menor = num;
                     continua = false;
-                } else if (num % div != 0) {
+                } else if (num % div != 0) {//Caso o número não seja divisivel ele incrementa duas vezes, assim o número sempre será positivo
                     num++;
                     num++;
                     break;

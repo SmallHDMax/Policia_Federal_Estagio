0d9da56697a886ac2e1169d214e83815818f11ed
SmallHDMax <50238083+SmallHDMax@users.noreply.github.com>
2019-08-03 00:43:15 -0300

    Update problem_25.java

--- Policia_Federal/src/policia_federal/problem_25.java
+++ Policia_Federal/src/policia_federal/problem_25.java
@@ -1,7 +1,7 @@
 public class problem_25 {
     public void fibonacci(){
-        int fib1 = 0, fib2 = 1, aux ,count  = 1;
-        while(Integer.toString(fib2).length() <4){
+        int fib1 = 0, fib2 = 1, aux ,count  = 1;//Variaveis para funcionamento da sequencia de Fibonacci
+        while(Integer.toString(fib2).length() <4){//Transformo o inteiro em String para poder saber seu tamanho e parar o while
             aux = fib2;
             fib2 = fib2 +fib1 ;
             fib1 = aux;

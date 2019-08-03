bfa1297de18332b8063f7cb6713cc69f0f52a1eb
SmallHDMax <50238083+SmallHDMax@users.noreply.github.com>
2019-08-03 00:37:40 -0300

    Update problem_25.java

--- Policia_Federal/src/policia_federal/problem_25.java
+++ Policia_Federal/src/policia_federal/problem_25.java
@@ -1,14 +1,13 @@
-package policia_federal;
-
 public class problem_25 {
     public void fibonacci(){
-        int fib1 = 0, fib2 = 1, aux;
+        int fib1 = 0, fib2 = 1, aux ,count  = 1;
         while(Integer.toString(fib2).length() <4){
             aux = fib2;
             fib2 = fib2 +fib1 ;
             fib1 = aux;
+            count++;
         }
-        System.out.println("Primeiro número com 4 digitos é :"+fib2);
+        System.out.println("Primeiro número com 4 digitos é : "+fib2+" Seu indicie é : "+count);
         
     }
 }
